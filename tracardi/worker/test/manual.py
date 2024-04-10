f55c9fcc8db7a1fd2259371d4e61c301ed7a2736
risto <risto.kowaczewski@gmail.com>
2024-04-10 13:51:19 +0200

    Update versions to 0.9.0-rc6.

--- tracardi/worker/test/manual.py
+++ tracardi/worker/test/manual.py
@@ -31,7 +31,7 @@ async def main():
         },
         "params": {
             "mysql": "user",
-            "version": "0.9.0-rc5"
+            "version": "0.9.0-rc6"
         },
         "worker": "copy_to_mysql",
         "asynchronous": True
