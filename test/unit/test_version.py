7ea37fe7bacc496908b6d994851a9f83800ef377
risto <risto.kowaczewski@gmail.com>
2024-04-22 11:39:56 +0200

    Changed to version 0.9.0

--- test/unit/test_version.py
+++ test/unit/test_version.py
@@ -2,5 +2,5 @@ from tracardi.domain.version import Version
 
 
 def test_version_name():
-    version = Version(version="0.9.0-dev", db_version="090x")
+    version = Version(version="0.9.0", db_version="090x")
     assert version.name == Version._generate_name("090x")
