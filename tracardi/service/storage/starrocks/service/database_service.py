8489ba49014a4df2cf8f9748cab224667ac4bf00
risto <risto.kowaczewski@gmail.com>
2024-05-17 07:24:58 +0200

    feat(storage): add StarRocks database service and engine functionality.

---
+++ tracardi/service/storage/starrocks/service/database_service.py
@@ -0,0 +1,45 @@
+from sqlalchemy import text
+
+from tracardi.config import starrocks
+from tracardi.service.storage.mysql.schema.table import Base
+from tracardi.service.storage.starrocks.engine import AsyncStarRocksEngine
+
+
+class DatabaseService:
+
+    def __init__(self):
+        self.client = AsyncStarRocksEngine()
+
+    async def _create_tables(self):
+        engine = self.client.get_engine_for_database()
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+            await conn.commit()
+        await engine.dispose()
+
+    async def _create_database(self):
+        engine = self.client.get_engine()
+        async with engine.connect() as conn:
+            await conn.execute(text(f"CREATE DATABASE IF NOT EXISTS `{starrocks.starrocks_database}`"))
+            await conn.commit()
+        await engine.dispose()
+
+    async def exists(self, database_name: str) -> bool:
+        engine = self.client.get_engine()
+        async with engine.connect() as conn:
+            result = await conn.execute(text(f"SHOW DATABASES LIKE '{database_name}';"))
+            return result.fetchone() is not None
+
+    async def bootstrap(self):
+
+        # Connect to the database
+        await self._create_database()
+
+        # Create a new async engine instance with the database selected
+        await self._create_tables()
+
+    async def drop(self, database_name):
+        engine = self.client.get_engine()
+        async with engine.connect() as conn:
+            await conn.execute(text(f"DROP DATABASE IF EXISTS `{database_name}`;"))
+            await conn.commit()
