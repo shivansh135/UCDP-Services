bb681cdd6e7ebae3228bb827209347b743b29d0f
risto <risto.kowaczewski@gmail.com>
2024-05-17 07:24:38 +0200

    feat: Add SAVE_LOGS_ENV_CONFIGURATION to available configuration list.

--- tracardi/service/setup/setup_configuration.py
+++ tracardi/service/setup/setup_configuration.py
@@ -1,6 +1,7 @@
 from tracardi.domain.configuration import Configuration
 
 GITHUB_CONFIGURATION = '9740f93e-66b2-4016-8bf1-9c9ed14cb226'
+SAVE_LOGS_ENV_CONFIGURATION = 'SAVE_LOGS'
 
 available_configuration_list = {
     GITHUB_CONFIGURATION: Configuration(
@@ -13,6 +14,16 @@ available_configuration_list = {
         },
         description="Repository on GitHub to store Tracardi workflows.",
         enabled=True,
-        tags=['github']
-    )
+        tags=['github'],
+        # cluster_wide_value = True
+    ),
+    SAVE_LOGS_ENV_CONFIGURATION: Configuration(
+        id=SAVE_LOGS_ENV_CONFIGURATION,
+        name=f"{SAVE_LOGS_ENV_CONFIGURATION} Env Variable",
+        config="yes",  # default value
+        description="When set to yes all logs will be saved in tracardi log.",
+        enabled=True,
+        tags=['env', 'setting'],
+        # cluster_wide_value = True
+    ),
 }
