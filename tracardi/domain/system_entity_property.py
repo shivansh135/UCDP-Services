8ebbadf634f500d811bffd8c619e00def1b9af43
risto <risto.kowaczewski@gmail.com>
2024-06-26 19:28:09 +0200

    refactoring of merging.

--- tracardi/domain/system_entity_property.py
+++ tracardi/domain/system_entity_property.py
@@ -1,6 +1,10 @@
-from typing import Optional, List
+from typing import Optional, List, Set
 
 from tracardi.domain.entity import Entity
+
+
+def get_wildcard(path):
+    return f"{path}.*"
 
 
 class SystemEntityProperty(Entity):
@@ -11,5 +15,24 @@ class SystemEntityProperty(Entity):
     default: Optional[str] = None
     optional: bool = False
     converter: Optional[str] = None
-    merge_strategies: List[str]
+    merge_strategies: Optional[List[str]]
     nested: Optional[bool] = False
+    undefined: Optional[bool] = False
+
+    def wildcard_property(self) -> bool:
+        return self.property == get_wildcard(self.path)
+
+    def property_in(self, properties) -> bool:
+        return self.property in properties
+
+    def is_property_of(self, fields, in_path) -> bool:
+        return (self.property_in(fields) or self.wildcard_property()) and self.path == in_path
+
+    def __hash__(self):
+        return hash(f"{self.path}-{self.property}")
+
+
+class SystemEntityPropertySet(Set[SystemEntityProperty]):
+
+    def __str__(self):
+        return f"SystemEntityPropertySet({[item.property for item in self]})"
