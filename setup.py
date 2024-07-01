670c657b630abe86f4b59fa660e4d6a5b2819c2f
risto <risto.kowaczewski@gmail.com>
2024-07-01 15:02:08 +0200

    fix: remove outdated comments and update versions to 1.0.0
    - Remove "Change to 1.1.0-dev" comments from requirements.txt files
    - Update version from 1.0.1 to 1.0.0 in setup.py and other files
    - Ensure consistency across all relevant project files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setup(
     name='tracardi',
-    version='1.0.1',
+    version='1.0.0',
     description='Tracardi Customer Data Platform backend',
     long_description=long_description,
     long_description_content_type="text/markdown",
